b159ec5e3d75aceed376be758a3677d76c27ba5a
CarlosBazanta <78313652+CarlosBazanta@users.noreply.github.com>
2022-08-18 11:44:20 -0400

    Se agrego una pantalla de carga Al iniciar el programa se colocara una pantalla de carga, donde se actualizan o cargan los datosl. Se implemento una animacion donde los se coloca un punto en el enunciado que sale abajo del spinners, al llegar a 3 puntos se reinicia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Deletefront';
+
+  public load: boolean = false;
+  public loading: string = 'Cargando.';
+
+
+  ngOnInit(): void {
+    this.changeLoad()
+    setTimeout(()=>{this.load=true;},5000)
+  }
+
+  changeLoad(){
+
+      setInterval(()=>{this.loading += '.';
+      if(this.loading==='Cargando....')
+      {this.loading='Cargando.';}},600)
+
+  }
+
+
 }
